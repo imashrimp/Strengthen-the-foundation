2d2b48357d41fb334dbc0087f4603cf24afbb9ea
imashrimp <kwonhs8287@gmail.com>
2023-04-02 20:48:36 +0900

    done

--- leagueOfLegend/leagueOfLegend/MainViewController.swift
+++ leagueOfLegend/leagueOfLegend/MainViewController.swift
@@ -100,11 +100,14 @@ class Champion {
     }
     
     func buyItem(item: ItemList) {
-        offensePower += item.itemIBought.extraPower
-        gold -= item.itemIBought.expense
-        itemArray.append(item.itemIBought)
-        print("\(name)(이)가 \(item.itemIBought.name)을 구입했습니다. 공격력이 \(item.itemIBought.extraPower)(이)가 추가되어 \(offensePower)가 되었습니다.")
-        itemArray.append(item.itemIBought)
+        if gold >= item.itemIBought.expense {
+            itemArray.append(item.itemIBought)
+            offensePower += item.itemIBought.extraPower
+            gold -= item.itemIBought.expense
+            print("\(name)(이)가 \(item.itemIBought.name)을 구입했습니다. 공격력이 \(item.itemIBought.extraPower)(이)가 추가되어 \(offensePower)가 되었습니다.")
+        } else {
+            print("골드가 부족합니다")
+        }
     }
 }
 
@@ -144,64 +147,82 @@ class SelectChampScreen {
 }
 
 class InGame {
-// 타임은 시간 표시 하려고 넣었었나? 이유가 기억이 안 나내?
+// 타임은 시간 표시 하려고 넣었었음 => 게임 시간
     var time: Int
     var minionNumber: Int
     var minionAlive: [Minion] = []
     var timer: Timer?
-    
+
     init(time: Int, minionNumber: Int) {
         self.time = time
         self.minionNumber = minionNumber
     }
-    
+
     //    챔피언 인스턴스가 하나 있겠지? 왜냐하면 챔피언을 고르고 게임을 시작한 상태가 인게임 상태이니까 => 근데 이 챔피언이 'SelectChampScreen' 클래스의 'selectChampion'에 의해 만들어진 챔피언이야 함
 // myChampion은 징크스다
     var nexus: Nexus = .init()
+    // 이거를 바꿔야하지 않을까? => 메서드를 통해 챔피언이 생성되도록 가령 selectChampionScreen에 있는 selectChampion 메서드처럼
     var myChampion = SelectChampScreen().selectChampion(selected: .jinx)
 
-    
-    func startGameTime() {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(giveGold), userInfo: nil, repeats: true)
+    func startGame() {
+        timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(giveGold), userInfo: nil, repeats: true)
+        showGametime()
+        spawnMinionOn30sec()
     }
     
+    func showGametime() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(showMinAndSec), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showMinAndSec() {
+        time += 1
+        
+        let minute = time / 60
+        let second = time % 60
+        
+        print(String(format: "게임 시간: %02d:%02d", minute, second))
+    }
+
     @objc func giveGold() {
         // 챔피언의 골드를 받아와서 +5 씩 주자
         myChampion.gold += 5
         print("\(myChampion.name)(이)가 초당 5의 골드를 자동으로 얻습니다.")
         // 뭔가 골드가 totalgold이런게 있어야 할거 같은 느낌이 드는데 한편으로는 jinx.gold가 징크스 라는 챔피언의 지갑? 같은 개념이라서 괜찮을거 같기도?
     }
-    
+
     // 여기서 확인해야 될게 미이언이 생성되고 30초마다 미니언 배열에 요소로 미니언이 3마리씩 들어가는지 확인
     func spawnMinionOn30sec() {
         // 이렇게 하면 타이머가 겹치거나 충돌이 일어나지 않는가?
         timer = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(spawnMinion), userInfo: nil, repeats: true)
         print(minionAlive)
     }
-    
+
     @objc func spawnMinion() {
         // createMinion 메서드가 뱉어내는 걸 array에 추가해야함
         minionAlive.append(contentsOf: nexus.createMinion())
         print("미니언이 3마리 생성되었습니다.")
     }
-    
+
     func countMinionNumber() {
         minionNumber = minionAlive.count
         print("미니언이 \(minionNumber)마리가 있습니다")
     }
-    
+
     //아이템을 사고
     func shopping(item: ItemList) {
         myChampion.buyItem(item: item)
     }
-    
+
+    // 아이템 배열을 콘솔에 프린트 할 수 있는 방법?
     func showMyStatus() {
-        print("\(myChampion.name)(은)는 \(myChampion.gold)를 갖고 있고, 공격력은\(myChampion.offensePower)이며, 아이템으로 \(myChampion.itemArray)를 갖고 있습니다.")
+        print("\(myChampion.name)(은)는 \(myChampion.gold)를 갖고 있고, 공격력은 \(myChampion.offensePower)이며, 아이템으로 \(myChampion.itemArray)를 갖고 있습니다.")
     }
-    
+
 //    => 'champAttackMinion'와 'champAttackNexus'는 공격하기 버튼을 누르면 작동
+
     func champAttackMinion() {
-        // 이 메서드의 실행 조건은 'if minionAlive != 0 { .champAttackMinion }' 이렇게 되어야함
+        print("champAttackMinion 실행됨")
+        // 이 메서드의 실행 조건은 'if minionAlive.count != 0 { .champAttackMinion }' 이렇게 되어야함
         if minionAlive[0].hp >= 0 {
             minionAlive[0].hp -= myChampion.offensePower
             print("\(myChampion.name)이 미니언을 공격해 \(myChampion.offensePower)의 데미지를 줬습니다. 미니언의 체력이 \(minionAlive[0].hp) 남았습니다")
@@ -211,17 +232,30 @@ class InGame {
             print("미니언 한 마리가 처치되었습니다.")
         }
     }
-    
+
     func champAttackNexus() {
-        // 이 함수의 실행 조건은 ' if minionAlive == 0 { .cahmpAttack. }'이렇게 되어야함
-        nexus.hp -= myChampion.offensePower
-        print("\(myChampion.name)이 넥서스를 공격해 \(myChampion.offensePower)의 데미지를 줬습니다. 넥서스의 체력이 \(nexus.hp)남았습니다.")
+        print("champAttackNexus 실행됨!")
+        // 이 함수의 실행 조건은 ' if minionAlive.count == 0 { .cahmpAttack. }'이렇게 되어야함
+        if nexus.hp > 0 {
+            nexus.hp -= myChampion.offensePower
+            print("\(myChampion.name)이 넥서스를 공격해 \(myChampion.offensePower)의 데미지를 줬습니다. 넥서스의 체력이 \(nexus.hp)남았습니다.")
+        } else {
+            gameFinished()
+        }
+            }
+
+    func doAttack() {
+        print("doAttack 실행됨")
+        if minionAlive.count > 0 {
+            champAttackMinion()
+        } else if minionAlive.count == 0 {
+            champAttackNexus()
+        }
     }
-    
+
     func gameFinished() {
         // 넥서스 체력이 0이 되면 시간이 멈추고 게임이 끝났다는 화면이 떠야함
         // 제어문 같은거(아마 챔피언의 공격 메서드 내부의 제어문 일듯?)에서 'if nexus.hp < 0 { .gameFinished() }'로 이 메서드가 호출 될 듯 => 이 메서드는 꼭 여기에 위치할 필요가 없음
-        // 시간이 멈춰야겠지?
         timer?.invalidate()
         print("게임이 끝났습니다.")
     }
